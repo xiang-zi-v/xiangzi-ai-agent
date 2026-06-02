faf81bf02ff2eda6fc9d9535fb88b098cb4aff1d
21914 <2191413700@qq.com>
2026-06-02 22:14:42 +0800

    添加 DocumentLoader

---
+++ src/main/java/rag/LoveAppDocumentLoader.java
@@ -0,0 +1,54 @@
+package rag;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.markdown.MarkdownDocumentReader;
+import org.springframework.ai.reader.markdown.config.MarkdownDocumentReaderConfig;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Slf4j
+public class LoveAppDocumentLoader {
+
+    private final ResourcePatternResolver resourcePatternResolver;
+
+
+    /**
+     * LoveAppDocumentLoader 构造函数
+     * 
+     * @param resourcePatternResolver 资源模式解析器，用于解析 classpath 下的文档资源
+     */
+    public LoveAppDocumentLoader(ResourcePatternResolver resourcePatternResolver) {
+        this.resourcePatternResolver = resourcePatternResolver;
+    }
+
+
+    public List<Document> loadDocuments() {
+        List<Document> documents = new ArrayList<>();
+        try {
+            Resource[] resources = this.resourcePatternResolver.getResources("classpath:document/*.md");
+            for (Resource resource : resources) {
+                String filename = resource.getFilename();
+                MarkdownDocumentReaderConfig config = MarkdownDocumentReaderConfig.builder()
+                        .withHorizontalRuleCreateDocument(true) // 使用水平规则创建文档
+                        .withIncludeCodeBlock(false) // 包含代码块
+                        .withIncludeBlockquote(false) // 包含引用块
+                        .withAdditionalMetadata("filename", filename) // 添加文件名元数据
+                        .build();
+                MarkdownDocumentReader reader = new MarkdownDocumentReader(resource, config);
+                documents.addAll(reader.get());
+            }
+        } catch (IOException e) {
+            log.error("加载文档失败", e);
+        }
+        return documents;
+    }
+
+
+}
